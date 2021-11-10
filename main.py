8656aca30bb6c3d3bfa68ae0cc46216b821fb3c2
Tejas <47889755+tejas15802@users.noreply.github.com>
2021-11-11 00:09:58 +0530

    Database and Table Initialization Works!

--- main.py
+++ main.py
@@ -1,15 +1,94 @@
-import mysql.connector as connector
+import mysql.connector
+from mysql.connector import errorcode
+
+DB_NAME = 'inventory'
+TABLES = {}
+TABLES['users'] = (
+"""   CREATE TABLE `inventory`.`user` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `roleId` SMALLINT NOT NULL,
+  `firstName` VARCHAR(50) NULL DEFAULT NULL,
+  `middleName` VARCHAR(50) NULL DEFAULT NULL,
+  `lastName` VARCHAR(50) NULL DEFAULT NULL,
+  `username` VARCHAR(50) NULL DEFAULT NULL,
+  `mobile` VARCHAR(15) NULL,
+  `email` VARCHAR(50) NULL,
+  `passwordHash` VARCHAR(32) NOT NULL,
+  `registeredAt` DATETIME NOT NULL,
+  `lastLogin` DATETIME NULL DEFAULT NULL,
+  `intro` TINYTEXT NULL DEFAULT NULL,
+  `profile` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `uq_username` (`username` ASC),
+  UNIQUE INDEX `uq_mobile` (`mobile` ASC),
+  UNIQUE INDEX `uq_email` (`email` ASC) );"""
+)
+
+TABLES['product'] = (
+   """CREATE TABLE `inventory`.`product` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(75) NOT NULL,
+  `summary` TINYTEXT NULL,
+  `type` SMALLINT(6) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) 
+);"""
+)
+
+def create_database(cursor):
+    
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except:
+        print("Database {}: already exist.".format(DB_NAME))
 
 
+    try:
+        cursor.execute("USE {}".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Database {} does not exists.".format(DB_NAME))
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            create_database(cursor)
+            print("Database {} created successfully.".format(DB_NAME))
+            con.database = DB_NAME
+        else:
+            print(err)
+            exit(1)
+
+
+
+def initialize(con): 
+    cursor = con.cursor()
+    create_database(cursor)
+    
+    for table_name in TABLES:
+        table_description = TABLES[table_name]
+        try:
+            print("Creating table {}: ".format(table_name), end='')
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
+    cursor.close()
+    con.close()
 
 if __name__ == '__main__':
     # TEST MYSQL CONNECTIONS
-    con = connector.connect(
+    con = mysql.connector.connect(
         host='localhost',
         user='root',
         passwd='root',
     )
     if con.is_connected():
         print('Connected to MySQL database')
+        initialize(con)
     else:
         print('Connection failed')
